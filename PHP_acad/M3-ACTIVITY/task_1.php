b684f2cd2566d0a7a8dc6cb6393884a8a7557d31
PrynsTag <201812311@fit.edu.ph>
2021-02-03 23:52:03 +0800

    Finished

---
+++ PHP_acad/M3-ACTIVITY/task_1.php
@@ -0,0 +1,13 @@
+<?php
+    $firstRow = array(
+        array("Prince Carl Velasco", "./images/my_image.jpeg", 21, "01/02/2014", "09514321351"),
+        array("Agnes Battiste", "./images/image_2.jpeg",43, "02/27/1977", "09514321351"),
+        array("Tracee Biehl", "./images/image_1.jpeg", 24, "12/26/1996", "09514321351"),
+        array("Lauran Messmer", "./images/image_2.jpeg", 35, "05/29/1985", "09514321351"),
+        array("Rossana Atkin", "./images/image_1.jpeg", 48, "04/13/1972", "09514321351"),
+        array("Rocky Zacarias", "./images/image_2.jpeg", 19, "05/13/2001", "09514321351"),
+        array("Quentin Grasso", "./images/image_1.jpeg", 45, "09/04/1975", "09514321351"),
+        array("Roxie Sprayberry", "./images/image_2.jpeg", 11, "11/29/2009", "09514321351"),
+        array("Lieselotte Krueger", "./images/image_1.jpeg", 15, "11/08/2005", "09514321351"),
+        array("Penney Baysinger", "./images/image_2.jpeg", 21, "02/24/1999", "09514321351")
+    );
